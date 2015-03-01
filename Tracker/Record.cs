d018c918e007df2a0582f2e88a7ab0d08c824437
Brains <completebrains@gmail.com>
2015-03-01 12:06:13 +0200

    Add Record class

---
+++ Tracker/Record.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracker
+{
+    public class Record
+    {
+        //------------------------------------------------------------------
+        public enum Categories
+        {
+            Food,
+            Health,
+            Housing,
+            Permanent,
+            Income,
+        }
+
+        //------------------------------------------------------------------
+        public enum Types
+        {
+            Income,
+            Cost,
+            Balance,
+        }
+
+        //------------------------------------------------------------------
+        private int ID;
+        private decimal amount;
+        private Categories categorie;
+        private string description;
+        private DateTime date;
+    }
+}
