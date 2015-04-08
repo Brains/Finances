f759bc7e26563b3f70b393a747fc54b69bddae1f
Brains <CompleteBrains@gmail.com>
2015-04-08 20:37:31 +0300

    Tracker

---
+++ Tracker/Expenses.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.Practices.Unity;
+using CodeContracts;
+
+namespace Tracker
+{
+    public class Expenses
+    {
+        //------------------------------------------------------------------
+        public List<Record> Records { get; private set; }
+
+        //------------------------------------------------------------------
+        public Expenses ()
+        {
+            Records = new List<Record> ();
+        }
+
+        //------------------------------------------------------------------
+        public void Add (Record record)
+        {
+            Requires.NotNull(record, "record");
+
+            Records.Add (record);
+        }
+    }
+}
--- Tracker/Record.cs
+++ Tracker/Record.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CodeContracts;
 
 namespace Tracker
 {
     public class Record
     {
         //------------------------------------------------------------------
-        public enum Categories
+	    public enum Types
+	    {
+		    Expense,
+		    Income,
+		    Balance,
+		    Debt,
+	    }
+
+		//------------------------------------------------------------------
+		public enum Categories
         {
             Food,
             Health,
@@ -18,19 +28,27 @@ namespace Tracker
             Income,
         }
 
-        //------------------------------------------------------------------
-        public enum Types
-        {
-            Income,
-            Cost,
-            Balance,
-        }
+	    //------------------------------------------------------------------
+        public int ID { get; private set; }
+        public Types Type { get; private set; }
+        public decimal Amount { get; private set; }
+        public Categories Categorie { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Date { get; private set; }
 
         //------------------------------------------------------------------
-        private int ID;
-        private decimal amount;
-        private Categories categorie;
-        private string description;
-        private DateTime date;
+        public Record (int id, decimal amount, Types type, Categories categorie, string description, DateTime date)
+        {
+			Requires.True(amount > 1, "amount > 1");
+			Requires.NotNullOrEmpty(description, "Description");
+			Requires.True(date > DateTime.Now.AddHours(-1), "date > DateTime.Now");
+
+	        ID = id;
+            Type = type;
+            Amount = amount;
+            Categorie = categorie;
+            Description = description;
+            Date = date;
+        }
     }
 }
