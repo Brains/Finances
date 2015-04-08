ea353e3b3fb093670545306dc8715db1ce629da0
Brains <CompleteBrains@gmail.com>
2015-04-08 20:31:16 +0300

    Add Tracker.Tests

---
+++ Tracker.UnitTests/ExpensesTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tracker.UnitTests
+{
+    [TestFixture]
+    public class ExpensesTests
+    {
+        //------------------------------------------------------------------
+        [Test]
+        public void Create_InvalidRecord_ReturnsException ()
+        {
+			Assert.Catch<ArgumentException>(() => new Record(333, 0, Record.Types.Balance, Record.Categories.Food, "Test", DateTime.Now));
+			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, null, DateTime.Now));
+			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, "", DateTime.Now));
+			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, "asdd", DateTime.Now.AddDays(-1)));
+		}
+
+		//------------------------------------------------------------------
+		[Test]
+        public void AddRecord_Empty_NotFailed ()
+        {
+            Expenses expenses = new Expenses();
+
+	        Assert.Catch<ArgumentNullException>(() => expenses.Add(null));
+        }
+    }
+}
