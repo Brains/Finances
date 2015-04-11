d966a2166a709581fc5a4b53d19b03d8ad4fd948
Brains <CompleteBrains@gmail.com>
2015-04-11 04:35:23 +0300

    Update Tests

--- Tracker.UnitTests/ExpensesTests.cs
+++ Tracker.UnitTests/ExpensesTests.cs
@@ -8,16 +8,18 @@ using NUnit.Framework;
 namespace Tracker.UnitTests
 {
     [TestFixture]
-    public class ExpensesTests
+    public class ExpensesTests : AssertionHelper
     {
         //------------------------------------------------------------------
         [Test]
         public void Create_InvalidRecord_ReturnsException ()
         {
+			Expect(() => new Record(333, 0, Record.Types.Balance, Record.Categories.Food, "Test", DateTime.Now), Throws.ArgumentException);
 			Assert.Catch<ArgumentException>(() => new Record(333, 0, Record.Types.Balance, Record.Categories.Food, "Test", DateTime.Now));
 			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, null, DateTime.Now));
 			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, "", DateTime.Now));
-			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, "asdd", DateTime.Now.AddDays(-1)));
+//			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, "asdd", DateTime.Now.AddDays(-1)));
+
 		}
 
 		//------------------------------------------------------------------
@@ -28,20 +30,5 @@ namespace Tracker.UnitTests
 
 	        Assert.Catch<ArgumentNullException>(() => expenses.Add(null));
         }
-
-	    //------------------------------------------------------------------
-	    [Test]
-	    public void Test ()
-	    {
-            Expenses expenses = new Expenses();
-
-			expenses.Save();
-
-		    Console.WriteLine((Record.Types)0);
-		    Console.WriteLine((Record.Types)1);
-		    Console.WriteLine((Record.Types)3);
-		    Console.WriteLine((Record.Types)4);
-
-		}
 	}
 }
