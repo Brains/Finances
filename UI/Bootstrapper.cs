e67a971775f6ba3e627c917cd427fe4c9255e218
Brains <CompleteBrains@gmail.com>
2015-04-11 21:30:28 +0300

    Add UI with Prism

---
+++ UI/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Prism;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+
+namespace UI
+{
+	class Bootstrapper : UnityBootstrapper
+	{
+		protected override DependencyObject CreateShell ()
+		{
+			return Shell;
+//			return ServiceLocator.Current.GetInstance<Shell>();
+		}
+
+		//------------------------------------------------------------------
+		protected override void InitializeShell ()
+		{
+			Application.Current.MainWindow = (Window) Shell;
+			Application.Current.MainWindow.Show();
+		}
+	}
+}
