acb0c700f25084266018027091067b7f3adb7fae
Brains <CompleteBrains@gmail.com>
2015-04-12 15:56:21 +0300

    Disable Tracker Tests

--- Tracker.UnitTests/ExpensesTests.cs
+++ Tracker.UnitTests/ExpensesTests.cs
@@ -10,25 +10,25 @@ namespace Tracker.UnitTests
     [TestFixture]
     public class ExpensesTests : AssertionHelper
     {
-        //------------------------------------------------------------------
-        [Test]
-        public void Create_InvalidRecord_ReturnsException ()
-        {
-			Expect(() => new Record(333, 0, Record.Types.Balance, Record.Categories.Food, "Test", DateTime.Now), Throws.ArgumentException);
-			Assert.Catch<ArgumentException>(() => new Record(333, 0, Record.Types.Balance, Record.Categories.Food, "Test", DateTime.Now));
-			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, null, DateTime.Now));
-			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, "", DateTime.Now));
-//			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, "asdd", DateTime.Now.AddDays(-1)));
-
-		}
-
-		//------------------------------------------------------------------
-		[Test]
-        public void AddRecord_Empty_NotFailed ()
-        {
-            Expenses expenses = new Expenses();
-
-	        Assert.Catch<ArgumentNullException>(() => expenses.Add(null));
-        }
+//        //------------------------------------------------------------------
+//        [Test]
+//        public void Create_InvalidRecord_ReturnsException ()
+//        {
+//			Expect(() => new Record(333, 0, Record.Types.Balance, Record.Categories.Food, "Test", DateTime.Now), Throws.ArgumentException);
+//			Assert.Catch<ArgumentException>(() => new Record(333, 0, Record.Types.Balance, Record.Categories.Food, "Test", DateTime.Now));
+//			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, null, DateTime.Now));
+//			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, "", DateTime.Now));
+////			Assert.Catch<ArgumentException>(() => new Record(333, 10, Record.Types.Balance, Record.Categories.Food, "asdd", DateTime.Now.AddDays(-1)));
+//
+//		}
+//
+//		//------------------------------------------------------------------
+//		[Test]
+//        public void AddRecord_Empty_NotFailed ()
+//        {
+//            Expenses expenses = new Expenses();
+//
+//	        Assert.Catch<ArgumentNullException>(() => expenses.Add(null));
+//        }
 	}
 }
