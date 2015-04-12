9fd63a35541fe266ffa3c747c7f610de2dabbd3f
Brains <CompleteBrains@gmail.com>
2015-04-12 13:21:07 +0300

    Add TrackerModule

---
+++ Tracker/TrackerModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Unity;
+
+namespace Tracker
+{
+	class TrackerModule : IModule
+	{
+		private readonly IUnityContainer container;
+		//------------------------------------------------------------------
+		public TrackerModule (IUnityContainer container)
+		{
+			this.container = container;
+
+			container.RegisterType<>();
+		}
+
+		//------------------------------------------------------------------
+		public void Initialize ()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
