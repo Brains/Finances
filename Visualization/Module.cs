e97392125a965dd2869bc561c445fd11b68a30f4
Brains <CompleteBrains@gmail.com>
2015-04-14 21:18:24 +0300

    Visualization tab works

---
+++ Visualization/Module.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+
+namespace Visualization
+{
+	public class Module : IModule
+	{
+		private readonly IUnityContainer container;
+
+		//------------------------------------------------------------------
+		public Module (IUnityContainer container)
+		{
+			this.container = container;
+		}
+
+		//------------------------------------------------------------------
+		public void Initialize ()
+		{
+			var regionManager = container.Resolve<IRegionManager>();
+
+			regionManager.RegisterViewWithRegion("Visualization", () => this.container.Resolve<Views.Categories>());
+		}
+	}
+}
---
+++ Visualization/Views/Categories.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Visualization.Views
+{
+    public partial class Categories : UserControl
+    {
+	    //------------------------------------------------------------------
+        public Categories()
+        {
+            InitializeComponent();
+
+			chart.DataContext = new KeyValuePair<string, int>[] {
+									new KeyValuePair<string, int>("Dog", 30),
+									new KeyValuePair<string, int>("Cat", 25),
+									new KeyValuePair<string, int>("Rat", 5),
+									new KeyValuePair<string, int>("Hampster", 8),
+									new KeyValuePair<string, int>("Rabbit", 12) };
+		}
+    }
+}
