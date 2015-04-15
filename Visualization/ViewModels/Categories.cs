8f5777594787b3cfd4bbe45ad01fc66e5b159f04
Brains <CompleteBrains@gmail.com>
2015-04-15 12:38:53 +0300

    Add Visualization.ViewModels

---
+++ Visualization/ViewModels/Categories.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracker;
+
+namespace Visualization.ViewModels
+{
+	// For XAML
+	//------------------------------------------------------------------
+	public class MarkupDictionary : Dictionary<string, int> { }
+
+	class Categories
+	{
+		//------------------------------------------------------------------
+		public Dictionary<string, int> Data { get; }
+
+		//------------------------------------------------------------------
+		public Categories ()
+		{
+			Data = new Dictionary<string, int>();
+
+			var expenses = new Expenses().Records;
+
+			foreach (Record.Categories category in Enum.GetValues(typeof(Record.Categories)))
+			{
+				Data.Add(category.ToString(), Sum(expenses, category));
+			}
+
+		}
+
+		//------------------------------------------------------------------
+		private static int Sum (List<Record> expenses, Record.Categories type)
+		{
+			var asd = from record in expenses
+					  where record.Category == type
+					  select record.Amount;
+
+			return (int) asd.Aggregate((first, second) => first + second);
+		}
+	}
+}
--- Visualization/Views/Categories.xaml.cs
+++ Visualization/Views/Categories.xaml.cs
@@ -13,42 +13,18 @@ namespace Visualization.Views
 	    //------------------------------------------------------------------
         public Categories()
         {
-            InitializeComponent();
+			InitializeComponent();
 
-			Chart.DataContext = new KeyValuePair<string, int>[] {
-									new KeyValuePair<string, int>("Dog", 30),
-									new KeyValuePair<string, int>("Cat", 25),
-									new KeyValuePair<string, int>("Rat", 5),
-									new KeyValuePair<string, int>("Hampster", 8),
-									new KeyValuePair<string, int>("Rabbit", 12) };
+//			Chart.DataContext = new KeyValuePair<string, int>[] {
+//									new KeyValuePair<string, int>("Dog", 30),
+//									new KeyValuePair<string, int>("Cat", 25),
+//									new KeyValuePair<string, int>("Rat", 5),
+//									new KeyValuePair<string, int>("Hampster", 8),
+//									new KeyValuePair<string, int>("Rabbit", 12) };
 
-
-			var expenses = new Expenses().Records;
-
-
-			var sums = new Dictionary<string, int>();
-
-	        foreach (Record.Categories category in Enum.GetValues(typeof (Record.Categories)))
-	        {
-				sums.Add(category.ToString(), Sum(expenses, category));
-
-			}
-
-
-
-
-
-			Chart.DataContext = sums;
+	        Chart.DataContext = new ViewModels.Categories();
         }
 
-	    //------------------------------------------------------------------
-	    private static int Sum (List<Record> expenses, Record.Categories type)
-	    {
-		    var asd = from record in expenses
-			    where record.Category == type
-			    select record.Amount;
 
-		    return (int) asd.Aggregate((first, second) => first + second);
-	    }
     }
 }
