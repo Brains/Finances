69e76f75d02f2273af70b360d8eba1ce6c57db5e
Brains <CompleteBrains@gmail.com>
2015-04-15 20:01:07 +0300

    Add neat LINQ queries for extctracting Data

--- Visualization/ViewModels/Categories.cs
+++ Visualization/ViewModels/Categories.cs
@@ -9,35 +9,61 @@ namespace Visualization.ViewModels
 {
 	// For XAML
 	//------------------------------------------------------------------
-	public class MarkupDictionary : Dictionary<string, int> { }
+	public class MarkupDictionary : Dictionary<string, int> {}
 
-	class Categories
+	public class Categories
 	{
 		//------------------------------------------------------------------
-		public Dictionary<string, int> Data { get; }
+		public Dictionary<string, int> ExpencesByCategory { get; }
+		public Dictionary<string, int> ExpencesByDate { get; }
+		public Dictionary<string, int> ExpencesByType { get; set; }
 
 		//------------------------------------------------------------------
 		public Categories ()
 		{
-			Data = new Dictionary<string, int>();
+			ExpencesByCategory = new Dictionary<string, int>();
 
 			var expenses = new Expenses().Records;
 
-			foreach (Record.Categories category in Enum.GetValues(typeof(Record.Categories)))
-			{
-				Data.Add(category.ToString(), Sum(expenses, category));
-			}
+			ExpencesByCategory = GetExpencesByCategory(expenses);
+			ExpencesByDate = GetDatesData(expenses);
+			ExpencesByType = GetExpencesByType(expenses);
+		}
 
+
+		//------------------------------------------------------------------
+		public Dictionary<string, int> GetExpencesByCategory (List<Record> expenses)
+		{
+			var query = from record in expenses
+				group record by record.Category
+				into grouped
+				select new {Key = grouped.Key, Value = grouped.Sum(record => record.Amount)};
+
+			return query.ToDictionary(x => x.Key.ToString(), x => (int) x.Value);
 		}
 
 		//------------------------------------------------------------------
-		private static int Sum (List<Record> expenses, Record.Categories type)
+		public Dictionary<string, int> GetExpencesByType (List<Record> expenses)
 		{
-			var asd = from record in expenses
-					  where record.Category == type
-					  select record.Amount;
+			var query = from record in expenses
+				group record by record.Type
+				into grouped
+				select new {Key = grouped.Key, Value = grouped.Sum(record => record.Amount)};
 
-			return (int) asd.Aggregate((first, second) => first + second);
+			return query.ToDictionary(x => x.Key.ToString(), x => (int) x.Value);
+		}
+
+		//------------------------------------------------------------------
+		public Dictionary<string, int> GetDatesData (List<Record> expenses)
+		{
+			var query = from record in expenses
+				where record.Date.Month == 3
+				orderby record.Date.ToString("yy-MM-dd")
+				group record by record.Date.ToString("yy-MM-dd")
+				into grouped
+				select new {Key = grouped.Key, Value = grouped.Sum(record => record.Amount)};
+
+			return query.ToDictionary(x => x.Key, x => (int) x.Value);
 		}
 	}
 }
