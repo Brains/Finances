b6d19f488675f96f9144467e91a19a121d4eb5fd
Brains <CompleteBrains@gmail.com>
2015-04-15 16:20:07 +0300

    Add Visualization.Tests + Records.xml

---
+++ Tests/Visualization.Tests/ChardData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using NUnit.Framework;
+using Tracker;
+using Visualization.ViewModels;
+
+namespace Visualization.Tests
+{
+	[TestFixture]
+    public class ChardData : AssertionHelper
+    {
+		public List<Record> Records { get; set; } = new List<Record>();
+
+		[SetUp]
+		//------------------------------------------------------------------
+		public void LoadData ()
+		{
+            XmlSerializer serializer = new XmlSerializer(typeof (List<Record>));
+
+			using (StreamReader stream = new StreamReader("Records.xml"))
+			using (var reader = XmlReader.Create(stream))
+			{
+				Records = (List<Record>) serializer.Deserialize(reader);
+			}
+		}
+
+		//------------------------------------------------------------------
+		[Test]
+		public void Test ()
+		{
+			Categories categories = new Categories();
+
+			var actual = categories.GetDatesData(Records);
+
+			Dictionary<string, int> expected = new Dictionary<string, int> {["Expense"] = 25685, ["Income"] = 28785};
+			Expect(actual, EquivalentTo(expected));
+		}
+
+    }
+}
