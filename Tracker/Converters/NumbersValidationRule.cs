0c24ba96d5fc10b7ab107c41686fedb5b6f557f2
Brains <CompleteBrains@gmail.com>
2015-04-17 14:03:12 +0300

    Add NumbersValidationRule

---
+++ Tracker/Converters/NumbersValidationRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Tracker.Converters
+{
+	public class NumbersValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate (object value, CultureInfo cultureInfo)
+		{
+			string number = value?.ToString();
+
+			int integer;
+			bool result = int.TryParse(number, out integer);
+
+			if (!result)
+				return new ValidationResult(false, "CHEATER!!!");
+
+			return new ValidationResult(true, null);
+
+		}
+	}
+}
