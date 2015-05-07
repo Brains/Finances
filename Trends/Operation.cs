b2345e2c1bdfd63d321153be864906afa03b3909
Brains <CompleteBrains@gmail.com>
2015-05-07 17:30:53 +0300

    Add Trends project

---
+++ Trends/Operation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trends
+{
+	class Operation
+	{
+		public decimal Amount { get; set; }
+		public DateTime Start { get; set; }
+		public DateTime Recurrence { get; set; }
+		public string Description { get; set; }
+	}
+}
