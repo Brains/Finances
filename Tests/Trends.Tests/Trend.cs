517c58e47b774bc452cac5e26696577744ef7672
Brains <CompleteBrains@gmail.com>
2015-05-08 15:31:32 +0300

    Trend: Calendar calculation

--- Tests/Trends.Tests/Trend.cs
+++ Tests/Trends.Tests/Trend.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Linq;
 using NodaTime;
 using NUnit.Framework;
+using static System.Console;
 
 namespace Trends.Tests
 {
@@ -13,18 +14,9 @@ namespace Trends.Tests
 		[Test]
 		public void Test ()
 		{
-			Operation operation = new Operation 
-			{
-				Start = new LocalDate(2009, 10, 6),
-				Period = Period.FromMonths(1)
-			};
+			var trend = new Trends.Trend();
 
-			LocalDate date = operation.Start;
-			foreach (var index in Enumerable.Range(0, 5))
-			{
-				Console.WriteLine(date.ToString());
-				date = date + operation.Period;
-			}
+			trend.Calendar.ForEach(WriteLine);
 		}
 	}
 }
--- Trends/Trend.cs
+++ Trends/Trend.cs
@@ -1,6 +1,54 @@
-﻿namespace Trends
+﻿using System.Collections.Generic;
+using System.Linq;
+using NodaTime;
+
+namespace Trends
 {
 	public class Trend
 	{
+		private readonly Period monthly = Period.FromMonths(1);
+
+		//------------------------------------------------------------------
+		public List<Operation> Operations { get; set; }
+		public List<Transaction> Calendar { get; set; }
+
+		//------------------------------------------------------------------
+		public Trend ()
+		{
+			Operations = new List<Operation>
+			{
+				new Operation(-300, new LocalDate(2015, 1, 3), Period.FromDays(3), "Food"),
+				new Operation(-140, new LocalDate(2015, 1, 5), monthly, "Mozy"),
+				new Operation(-30, new LocalDate(2015, 1, 5), monthly, "O3"),
+				new Operation(-1800, new LocalDate(2015, 1, 15), monthly, "House"),
+				new Operation(1200, new LocalDate(2015, 1, 5), monthly, "Deposit"),
+				new Operation(200, new LocalDate(2015, 1, 7), monthly, "Deposit"),
+				new Operation(600, new LocalDate(2015, 1, 14), monthly, "Deposit"),
+			};
+
+			CreateCalendar();
+		}
+
+		//------------------------------------------------------------------
+		private void CreateCalendar ()
+		{
+			Calendar = new List<Transaction>();
+			
+
+			foreach (var operation in Operations)
+			{
+				LocalDate date = operation.Start;
+
+				while (date.Month < 3)
+				{
+                    Calendar.Add(new Transaction(operation.Amount, date, operation.Description));
+					date = date + operation.Period;
+				}
+			}
+
+			Calendar.Sort();
+		}
 	}
+
+
 }
