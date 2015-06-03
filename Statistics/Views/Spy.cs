15aa8f15aeeee17e2812a2ac14c908ca050726aa
CompleteBrains <CompleteBrains@gmail.com>
2015-06-03 12:10:08 +0300

    It WORKS !!!. Draft

---
+++ Statistics/Views/Spy.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Statistics.Views
+{
+	public class Spy : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
