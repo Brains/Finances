d7f4b319f8144bdf370f3a23daf198d49a5ad9f1
CompleteBrains <CompleteBrains@gmail.com>
2015-07-13 21:47:39 +0300

    LabelledTextBox: Replace implementation

---
+++ Statistics/Views/Resources/LabelledTextBox.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Statistics.Views.Resources
+{
+	/// <summary>
+	/// Interaction logic for LabelTextBox.xaml
+	/// </summary>
+	public partial class LabelledTextBox
+	{
+		public static readonly DependencyProperty LabelProperty = DependencyProperty
+			.Register("Label",
+					typeof(string),
+					typeof(LabelledTextBox),
+					new FrameworkPropertyMetadata("Unnamed Label"));
+
+		public static readonly DependencyProperty TextProperty = DependencyProperty
+			.Register("Text",
+					typeof(string),
+					typeof(LabelledTextBox),
+					new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+		public LabelledTextBox()
+		{
+			InitializeComponent();
+			Root.DataContext = this;
+		}
+
+		public string Label
+		{
+			get { return (string) GetValue(LabelProperty); }
+			set { SetValue(LabelProperty, value); }
+		}
+
+		public string Text
+		{
+			get { return (string) GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
+		}
+	}
+}
