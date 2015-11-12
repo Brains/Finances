3683c08a2638cb5f84b72d61d862c2e03692930d
CompleteBrains <CompleteBrains@gmail.com>
2015-11-12 20:29:35 +0200

    Add FormsQueue to Shell

--- Temporary/Shell/ShellViewModel.cs
+++ Temporary/Shell/ShellViewModel.cs
@@ -1,6 +1,15 @@
 using Caliburn.Micro;
+using Temporary.Records;
 
 namespace Temporary.Shell
 {
-	public class ShellViewModel : PropertyChangedBase, IShell {}
+	public class ShellViewModel : PropertyChangedBase, IShell
+	{
+		public ShellViewModel(FormsQueueViewModel forms)
+		{
+			Forms = forms;
+		}
+
+		public FormsQueueViewModel Forms { get; }
+	}
 }
