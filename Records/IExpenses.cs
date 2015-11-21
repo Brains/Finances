bf175e00fd091713d8a27c1d3889cea2be70ef7f
CompleteBrains <CompleteBrains@gmail.com>
2015-11-21 20:50:29 +0200

    Add RandomExpenses

--- Records/IExpenses.cs
+++ Records/IExpenses.cs
@@ -5,8 +5,7 @@ namespace Records
 {
 	public interface IExpenses
 	{
-		void Add (decimal amount, Record.Types type, Record.Categories category, string description, DateTime date);
 		ObservableCollection<Record> Records { get; set; }
-		void Load ();
+		void Add(decimal amount, Record.Types type, Record.Categories category, string description, DateTime date);
 	}
 }
