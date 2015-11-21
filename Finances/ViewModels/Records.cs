1c453516a4be5abef3319f58432d0b4b80fbb3d4
CompleteBrains <CompleteBrains@gmail.com>
2015-11-21 21:09:50 +0200

    Resources (+3 squashed commit)
    Squashed commit:
    
    [4281ee5] Templates
    
    [b076d11] Converters
    
    [00e70f6] Controls

--- Finances/ViewModels/Records.cs
+++ Finances/ViewModels/Records.cs
@@ -3,7 +3,6 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Data;
 using Caliburn.Micro;
-using Finances.Resources.Converters;
 using Records;
 
 namespace Finances.ViewModels
