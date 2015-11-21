2a66a2dc20b03abd6c4258808243fe4fa3bfbc40
CompleteBrains <CompleteBrains@gmail.com>
2015-11-21 20:55:00 +0200

    Records

--- Finances/ViewModels/Records.cs
+++ Finances/ViewModels/Records.cs
@@ -10,34 +10,7 @@ namespace Finances.ViewModels
 {
 	public class Records : PropertyChangedBase, IViewModel
 	{
-		public ObservableCollection<Record> RecordsList { get; }
 
 
-		// Model
-		private readonly IExpenses expenses;
-
-
-
-
-
-
-		public Records (IExpenses expenses)
-		{
-			this.expenses = expenses;
-			RecordsList = expenses.Records;
-
-			Group();
-		}
-
-		private void Group ()
-		{
-			ICollectionView view = CollectionViewSource.GetDefaultView(RecordsList);
-
-			view.SortDescriptions.Clear();
-            view.SortDescriptions.Add(new SortDescription("Date", ListSortDirection.Descending));
-			view.GroupDescriptions.Clear();
-			view.GroupDescriptions.Add(new PropertyGroupDescription("Date", new NumberToMonthConverter()));
-			view.GroupDescriptions.Add(new PropertyGroupDescription("Date", new DateTimeToDateConverter()));
-		}
 	}
 }
