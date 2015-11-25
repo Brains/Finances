b799d1aaf684108917d5fd985ed2b6864a2ac499
CompleteBrains <CompleteBrains@gmail.com>
2015-11-25 15:11:27 +0200

    Add Guard methods

---
+++ Finances/ViewModels/FormsQueue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Caliburn.Micro;
+using Microsoft.Practices.ObjectBuilder2;
+
+namespace Finances.ViewModels
+{
+	public class FormsQueue : Screen, IViewModel
+	{
+<<<<<<< HEAD
+=======
+		public List<Form> Forms { get; }
+
+		public void Add()
+		{
+			
+		}
+
+		public void Remove()
+		{
+
+		}
+
+		public void Submit()
+		{
+
+		}
+
+		public bool CanAdd() => false;
+		public bool CanRemove() => false;
+		public bool CanSubmit() => false;
+>>>>>>> Add Guard methods
+	}
+}
