0cbf8676725346c7155bc62b30c194aab6d76539
CompleteBrains <CompleteBrains@gmail.com>
2015-11-28 07:16:41 +0200

    CanAdd

--- Tests/UITests/ViewModels/FormsQueueTests.cs
+++ Tests/UITests/ViewModels/FormsQueueTests.cs
@@ -1,13 +1,34 @@
 ﻿using NUnit.Framework;
+using UI.ViewModels;
 
 namespace UITests.ViewModels
 {
-	public class FormsQueueTests
+	public class FormsQueueTests : AssertionHelper
 	{
 		[Test]
-		public void Test()
+		public void CanAdd_LessThanFourForms_ReturnsTrue()
 		{
-			
-		} 
+			var forms = new FormsQueue();
+
+			forms.Add();
+			var actual = forms.CanAdd();
+
+			Expect(actual, True);
+		}
+
+		[Test]
+		public void CanAdd_MoreThanFourForms_ReturnsFalse()
+		{
+			var forms = new FormsQueue();
+
+			forms.Add();
+			forms.Add();
+			forms.Add();
+			forms.Add();
+			forms.Add();
+			var actual = forms.CanAdd();
+
+			Expect(actual, False);
+		}
 	}
 }
--- UI/ViewModels/FormsQueue.cs
+++ UI/ViewModels/FormsQueue.cs
@@ -5,24 +5,24 @@ namespace UI.ViewModels
 {
 	public class FormsQueue : PropertyChangedBase, IViewModel
 	{
+		private const int Limit = 5;
 		public List<Form> Forms { get; }
+
+		public FormsQueue()
+		{
+			Forms = new List<Form>();
+		}
 
 		public void Add()
 		{
-			
+			Forms.Add(new Form());
 		}
 
-		public void Remove()
-		{
+		public void Remove() {}
 
-		}
+		public void Submit() {}
 
-		public void Submit()
-		{
-
-		}
-
-		public bool CanAdd() => false;
+		public bool CanAdd() => Forms.Count < Limit;
 		public bool CanRemove() => false;
 		public bool CanSubmit() => false;
 	}
