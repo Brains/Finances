dab954dbbed54c9e2c61ebb87fba14dd283b3bd4
CompleteBrains <CompleteBrains@gmail.com>
2015-12-02 13:38:11 +0200

    Form: filter Categories depending on selected Type

--- Tests/UITests/ViewModels/FormTests.cs
+++ Tests/UITests/ViewModels/FormTests.cs
@@ -1,20 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using Records;
 using UI.ViewModels;
+using static NSubstitute.Substitute;
 using static Records.Record;
+using static Records.Record.Categories;
+using static Records.Record.Types;
 
 namespace UITests.ViewModels
 {
 	public class FormTests : AssertionHelper
 	{
+		private static Form CreateForm()
+		{
+			var settings = For<ISettings>();
+
+			settings.CategoriesMapping = new Dictionary<Types, Categories[]>
+			{
+				[Expense] = new[] {Food, Health, House, General, Women, Other},
+				[Debt] =	new[] {Maxim, Andrey},
+				[Income] =	new[] {Deposit, ODesk},
+				[Shared] =	new[] {Food, House, General, Other},
+				[Balance] = new[] {Other}
+			};
+
+			return new Form(settings);
+		}
+
 		[Test]
 		public void Types_Always_ContainsAllMembersOfTypesEnum()
 		{
-			var form = new Form();
+			var form = CreateForm();
 
-			IEnumerable<Types> expected = new []{Types.Balance, Types.Debt, Types.Expense, Types.Income,  Types.Shared, } ;
+			IEnumerable<Types> expected = new []{Balance, Debt, Expense, Income,  Shared, } ;
 			Expect(form.Types, EquivalentTo(expected));
+		}
+
+		[TestCase(Expense,	new[] { Food, General, Health, House, Other, Women })]
+		[TestCase(Debt,		new[] { Maxim, Andrey })]
+		[TestCase(Income,	new[] { ODesk, Deposit })]
+		[TestCase(Shared,	new[] { Food, House, General, Other })]
+		[TestCase(Balance,	new[] { Other })]
+		public void SelectedTypeSetter_Always_FiltersAvailableCategories(Types type, Categories[] expected)
+		{
+			var form = CreateForm();
+
+			form.SelectedType = type;
+
+			Expect(form.Categories, EquivalentTo(expected));
 		}
 	}
 }
--- UI/ViewModels/Form.cs
+++ UI/ViewModels/Form.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Caliburn.Micro;
+using Records;
 using UI.Interfaces;
 using static Records.Record;
 
@@ -10,25 +10,46 @@ namespace UI.ViewModels
 {
 	public class Form : PropertyChangedBase, IForm
 	{
-		public int Amount { get; set; } 
+		private readonly ISettings settings;
+		private Types selectedType;
 		public IEnumerable<Types> Types { get; set; }
 		public IEnumerable<Categories> Categories { get; set; }
-		public Types SelectedType { get; set; }
+
+		public Types SelectedType
+		{
+			get { return selectedType; }
+			set
+			{
+				selectedType = value;
+				NotifyOfPropertyChange(nameof(SelectedType));
+				UpdateCategories(selectedType);
+			}
+		}
+
 		public Categories SelectedCategory { get; set; }
 		public string Description { get; set; }
 
-		public Form()
+		public Form(ISettings settings)
 		{
-			Types = Enum.GetValues(typeof (Types)).Cast<Types>();
-			Categories = Enum.GetValues(typeof(Categories)).Cast<Categories>();
+			this.settings = settings;
 
-			SelectedType = Types.First();
-			SelectedCategory = Categories.First();
+			Types = Enum.GetValues(typeof (Types)).Cast<Types>();
 		}
+
+		public int Amount { get; set; }
 
 		public void Submit()
 		{
 			throw new NotImplementedException();
+		}
+
+		private void UpdateCategories(Types type)
+		{
+			Categories = settings.CategoriesMapping[type];
+			SelectedCategory = Categories.First();
+
+			NotifyOfPropertyChange(nameof(Categories));
+			NotifyOfPropertyChange(nameof(SelectedCategory));
 		}
 	}
 }
