0e8ab9305179c8b6dcc36c88dac39057bb835515
CompleteBrains <CompleteBrains@gmail.com>
2015-12-04 04:32:00 +0200

    Add Group By Type

--- Tests/UITests/ViewModels/MonthDiagramsTests.cs
+++ Tests/UITests/ViewModels/MonthDiagramsTests.cs
@@ -1,7 +1,70 @@
-﻿namespace UITests.ViewModels
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using Records;
+using UI.ViewModels;
+using static NSubstitute.Substitute;
+using static Records.Record.Categories;
+using static Records.Record.Types;
+
+namespace UITests.ViewModels
 {
-	public class MonthDiagramsTests
+	public class MonthDiagramsTests : AssertionHelper
 	{
-		 
+		private IExpenses expenses;
+		private static readonly DateTime Date = DateTime.MinValue;
+
+		private MonthDiagrams Create()
+		{
+			var records = new[]
+			{
+				new Record(0, Expense, Food, "", Date),
+                new Record(0, Income, Food, "", Date),
+                new Record(0, Shared, Food, "", Date),
+                new Record(0, Debt, Food, "", Date),
+            };
+
+			expenses = For<IExpenses>();
+			expenses.Records = new ObservableCollection<Record>(records);
+
+			return new MonthDiagrams(expenses);
+		}
+
+		[Test]
+		public void GroupByType_Always_GroupsRecordsByType()
+		{
+			var diagrams = Create();
+			var expected = new Dictionary<Record.Types, Record[]>
+			{
+				[Expense]	= new []{new Record(0, Expense, Food, "", Date) },
+				[Income]	= new []{new Record(0, Income, Food, "", Date) },
+				[Shared]	= new []{new Record(0, Shared, Food, "", Date) },
+				[Debt]		= new []{new Record(0, Debt, Food, "", Date) },
+			};
+
+			var actual = diagrams.GroupByType(expenses.Records);
+
+			Expect(actual, EquivalentTo(expected));
+		}
+
+		private static Record Create(Record.Types type)
+		{
+			return new Record(0, type, Food, "", Date);
+		}
+		private static Record Create(Record.Categories category)
+		{
+			return new Record(0, Expense, category, "", Date);
+		}
+		private static Record Create(int amount)
+		{
+			return new Record(amount, Expense, Food, "", Date);
+		}
+		private static Record Create(string description)
+		{
+			return new Record(0, Expense, Food, description, Date);
+		}
+
 	}
 }
--- UI/ViewModels/MonthDiagrams.cs
+++ UI/ViewModels/MonthDiagrams.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using Caliburn.Micro;
 using Records;
 using UI.Interfaces;
+using Data = System.Collections.Generic.Dictionary<string, int>;
 
 namespace UI.ViewModels
 {
@@ -11,6 +14,22 @@ namespace UI.ViewModels
 		public MonthDiagrams(IExpenses expenses)
 		{
 			this.expenses = expenses;
+		}
+
+		public Dictionary<Record.Types, List<Record>> GroupByType(IEnumerable<Record> records)
+		{
+			return records.GroupBy(record => record.Type)
+			              .ToDictionary(grouping => grouping.Key, grouping => grouping.ToList());
+		}
+
+		public Data GetExpensesByType(IEnumerable<Record> records)
+		{
+			var query = from record in records
+						group record by record.Type
+						into grouped
+						select new { Key = grouped.Key, Value = grouped.Sum(record => record.Amount) };
+
+			return query.ToDictionary(x => x.Key.ToString(), x => (int) x.Value);
 		}
 	}
 }
