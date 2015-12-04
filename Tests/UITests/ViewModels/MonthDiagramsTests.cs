c4c01741f267ff1b8d16e78642c9b90cd49124f7
CompleteBrains <CompleteBrains@gmail.com>
2015-12-04 04:44:32 +0200

    Tests: reformat

--- Tests/UITests/ViewModels/MonthDiagramsTests.cs
+++ Tests/UITests/ViewModels/MonthDiagramsTests.cs
@@ -20,10 +20,10 @@ namespace UITests.ViewModels
 		{
 			var records = new[]
 			{
-				new Record(0, Expense, Food, "", Date),
-                new Record(0, Income, Food, "", Date),
-                new Record(0, Shared, Food, "", Date),
-                new Record(0, Debt, Food, "", Date),
+				Create(Expense),
+                Create(Income),
+                Create(Shared),
+                Create(Debt),
             };
 
 			expenses = For<IExpenses>();
@@ -32,39 +32,26 @@ namespace UITests.ViewModels
 			return new MonthDiagrams(expenses);
 		}
 
+		private static Record Create(Record.Types type) => new Record(10,  type, Food, "", Date);
+		private static Record Create(Record.Categories category) => new Record(10,  Expense, category, "", Date);
+		private static Record Create(int amount) => new Record(amount, Expense, Food, "", Date);
+		private static Record Create(string description) => new Record(10,  Expense, Food, description, Date);
+
 		[Test]
 		public void GroupByType_Always_GroupsRecordsByType()
 		{
 			var diagrams = Create();
 			var expected = new Dictionary<Record.Types, Record[]>
 			{
-				[Expense]	= new []{new Record(0, Expense, Food, "", Date) },
-				[Income]	= new []{new Record(0, Income, Food, "", Date) },
-				[Shared]	= new []{new Record(0, Shared, Food, "", Date) },
-				[Debt]		= new []{new Record(0, Debt, Food, "", Date) },
+				[Expense]	= new []{Create(Expense) },
+				[Income]	= new []{Create(Income) },
+				[Shared]	= new []{Create(Shared) },
+				[Debt]		= new []{Create(Debt) },
 			};
 
 			var actual = diagrams.GroupByType(expenses.Records);
 
 			Expect(actual, EquivalentTo(expected));
 		}
-
-		private static Record Create(Record.Types type)
-		{
-			return new Record(0, type, Food, "", Date);
-		}
-		private static Record Create(Record.Categories category)
-		{
-			return new Record(0, Expense, category, "", Date);
-		}
-		private static Record Create(int amount)
-		{
-			return new Record(amount, Expense, Food, "", Date);
-		}
-		private static Record Create(string description)
-		{
-			return new Record(0, Expense, Food, description, Date);
-		}
-
 	}
 }
