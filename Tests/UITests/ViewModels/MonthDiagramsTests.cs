f75897196a9a24ab0070c1adfe2ea71aa13ff7d7
CompleteBrains <CompleteBrains@gmail.com>
2015-12-05 21:07:36 +0200

    Tests: BalanceByMonth

--- Tests/UITests/ViewModels/MonthDiagramsTests.cs
+++ Tests/UITests/ViewModels/MonthDiagramsTests.cs
@@ -1,41 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using MoreLinq;
+using NSubstitute;
 using NUnit.Framework;
 using Records;
 using UI.ViewModels;
 using static NSubstitute.Substitute;
 using static Records.Record;
-using static Records.Record.Categories;
-using static Records.Record.Types;
 
 namespace UITests.ViewModels
 {
 	public class MonthDiagramsTests : AssertionHelper
 	{
-		private readonly Record[] records = AnalyzerTests.Records;
+		private readonly Record[] records = FixedRecords.Data;
+		private IAnalyzer analyzer;
+		private IExpenses expenses;
 
 		private MonthDiagrams Create()
 		{
-			return new MonthDiagrams(null, null);
+			var spending = new Dictionary<int, decimal>()
+			{
+				[10] = 300, 
+				[11] = 300, 
+				[12] = 300, 
+			};
+
+			var income = new Dictionary<int, decimal>()
+			{
+				[10] = 100, 
+				[11] = 100, 
+				[12] = 100, 
+			};
+
+			analyzer = For<IAnalyzer>();
+			expenses = For<IExpenses>();
+			expenses.Records = new ObservableCollection<Record>(records);
+			analyzer.CalculateTotalByMonth(null).ReturnsForAnyArgs(spending, income);
+
+			return new MonthDiagrams(expenses, analyzer);
 		}
 
 		private void Print<T>(IEnumerable<T> items) => items.ForEach(item => Console.WriteLine(item));
 
-
-		
-
 		[Test]
-		public void CalculateBalanceByMonth_Always_CalculateExpencesIncomeRationGroupedByMonth()
+		public void BalanceByMonth_Always_HasExpensesToIncomeRationGroupedByMonth()
 		{
 			var diagrams = Create();
 
-			var actual = diagrams.CalculateBalanceByMonth();
+			// Assserts
+			var keys = diagrams.BalanceByMonth.Select(pair => pair.Key);
+			var keysExpected = EquivalentTo(new[] {10, 11, 12});
+			var expense = diagrams.BalanceByMonth[Types.Expense];
+			var income = diagrams.BalanceByMonth[Types.Income];
 
-			Expect(actual, Count.EqualTo(3));
-			Expect(actual.Select(pair => pair.Key), EquivalentTo(new[] { 10, 11, 12}));
-			Expect(actual.Select(pair => pair.Value), All.EqualTo(500));
+			Expect(keys, EquivalentTo(new[] { Types.Expense, Types.Income}));
+			Expect(expense.Select(p => p.Key), keysExpected);
+			Expect(income.Select(p => p.Key), keysExpected);
+			Expect(expense.Select(p => p.Value), All.EqualTo(300));
+			Expect(income.Select(p => p.Value), All.EqualTo(100));
 		}
 	}
 }
