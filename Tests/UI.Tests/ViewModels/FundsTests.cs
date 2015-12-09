05f5c72489e19caa52706d849445ba257e50e8a7
CompleteBrains <CompleteBrains@gmail.com>
2015-12-09 18:19:51 +0200

    Add Divergence calculations

--- Tests/UI.Tests/ViewModels/FundsTests.cs
+++ Tests/UI.Tests/ViewModels/FundsTests.cs
@@ -1,13 +1,57 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using NSubstitute;
+using NUnit.Framework;
+using Records;
+using UI.Services;
 using UI.ViewModels;
+using static NSubstitute.Substitute;
+using static Records.Record.Types;
+using static Records.Record.Categories;
 
 namespace UI.Tests.ViewModels
 {
 	public class FundsTests
 	{
-		[Test]
-		public void Test()
+		private readonly Record[] records =
 		{
+			new Record(100, Expense,Food, "Novus",	new DateTime(2015, 10, 1)),
+			new Record(100, Shared,	Food, "Water",	new DateTime(2015, 10, 1)),
+			new Record(500, Income, Deposit, "",	new DateTime(2015, 10, 1)),
+		};
+
+		private static Funds Create()
+		{
+			var sources = Enumerable.Range(1, 3)
+			                        .Select(i =>
+			                        {
+				                        var source = For<IFundsSource>();
+				                        source.Value = 100;
+				                        return source;
+			                        })
+			                        .ToArray();
+
+			return new Funds(sources);
+		}
+
+		[Test]
+		public void CalculateEstimatedBalance_Always_ReturnsRestBasedOnRecords()
+		{
+			var funds = Create();
+
+			var actual = funds.CalculateEstimatedBalance(records);
+
+			Assert.That(actual, Is.EqualTo(300));
+		}
+
+		[Test]
+		public void CalculateDivergence_Always_ReturnsDifferenceInEstimatedAndRealBalance()
+		{
+			var funds = Create();
+
+			var actual = funds.CalculateDivergence(funds.Sources, records);
+
+			Assert.That(actual, Is.EqualTo(0));
 		}
 	}
 }
--- UI/ViewModels/Funds.cs
+++ UI/ViewModels/Funds.cs
@@ -1,19 +1,48 @@
-﻿using System.Linq;
+﻿using System;
+using System.Globalization;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using Records;
 using UI.Interfaces;
 using UI.Services;
+using static Records.Record.Types;
 
 namespace UI.ViewModels
 {
 	public class Funds : PropertyChangedBase, IViewModel
 	{
+		public IFundsSource[] Sources { get; }
+
 		public Funds(IFundsSource[] sources)
 		{
-			var values = sources.Select(source => source.Get());
+			Sources = sources;
+
+            if (!sources.Any()) throw new ArgumentException();
+		}
+
+		public decimal CalculateDivergence(IFundsSource[] sources, Record[] records)
+		{
+			var real = sources.Sum(source => source.Value);
+			var estimated = CalculateEstimatedBalance(records);
+
+			return real - estimated;
+		}
+
+
+		public decimal CalculateEstimatedBalance(Record[] records)
+		{
+			var types = records.ToLookup(record => record.Type)
+			                   .ToDictionary(type => type.Key,
+			                                 type => type.Sum(record => record.Amount));
+
+			var spending = types[Expense] + types[Shared];
+			var income = types[Income];
+
+			return income - spending;
 		}
 	}
 }
