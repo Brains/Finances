f9068e2c0cf72459e50890b7179046c284159011
CompleteBrains <CompleteBrains@gmail.com>
2015-12-09 19:33:57 +0200

    BankSourceTests: PullValue_Always_SetCorrectValue

--- Tests/Funds.Tests/BankSourceTests.cs
+++ Tests/Funds.Tests/BankSourceTests.cs
@@ -1,7 +1,17 @@
-﻿namespace Funds.Tests
+﻿using NUnit.Framework;
+
+namespace Funds.Tests
 {
 	public class BankSourceTests
 	{
-		 
+		[Test]
+		public void PullValue_Always_SetCorrectValue()
+		{
+			var source = new BankSource();
+
+			source.PullValue();
+
+			Assert.That(source.Value, Is.EqualTo(100));
+		}
 	}
 }
