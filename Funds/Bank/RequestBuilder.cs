5d099c27d778460d635fa72b810c6cbeb8e66178
CompleteBrains <CompleteBrains@gmail.com>
2015-12-09 20:54:10 +0200

    Add RequestBuilder

---
+++ Funds/Bank/RequestBuilder.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Funds.Bank;
+// ReSharper disable PossibleNullReferenceException
+
+namespace Funds.Bank
+{
+	public struct PersonalData
+	{
+		public readonly string ID;
+		public readonly string Card;
+		public readonly string Password;
+
+		public PersonalData(string id, string password, string card)
+		{
+			ID = id;
+			Password = password;
+			Card = card;
+		}
+	}
+
+	public class RequestBuilder
+	{
+		private readonly IEncryption encryption;
+
+		public RequestBuilder(IEncryption encryption)
+		{
+			this.encryption = encryption;
+		}
+
+		public string Repair(XElement file)
+		{
+			var pattern = @"[^\x20-\x7e]";
+			return Regex.Replace(file.ToString(SaveOptions.DisableFormatting), pattern, string.Empty);
+		}
+
+		public string Build(string xml, PersonalData personal)
+		{
+			xml = InsertPersonalData(xml, personal);
+			xml = InsertDatesRange(xml, DateTime.Now.AddDays(-5), DateTime.Now);
+			var file = PutSignature(xml, personal);
+
+			return file.ToString(SaveOptions.DisableFormatting);
+		}
+
+		public string InsertPersonalData(string xml, PersonalData personal)
+		{
+			XElement file = XElement.Parse(xml);
+
+			file.Element("merchant").SetElementValue("id", personal.ID);
+
+			file.Descendants("prop")
+				.Single(e => e.Attribute("name").Value == "card")
+				.SetAttributeValue("value", personal.Card);
+
+			return file.ToString(SaveOptions.DisableFormatting);
+
+		}
+
+		private XElement PutSignature(string xml, PersonalData personal)
+		{
+			var data = ExtractData(xml);
+			var signature = encryption.CalculateSignature(data + personal.Password);
+			var file = InsertSignature(xml, signature);
+			return file;
+		}
+
+		private string ExtractData(string xml)
+		{
+			XElement file = XElement.Parse(xml);
+
+			var data = new StringBuilder();
+
+			foreach (var element in file.Element("data").Nodes())
+				data.Append(element.ToString(SaveOptions.DisableFormatting));
+
+			return data.ToString();
+		}
+
+		private XElement InsertSignature(string xml, string signature)
+		{
+			XElement file = XElement.Parse(xml);
+			var signatureElement = file.Element("merchant").Element("signature");
+			signatureElement.SetValue(signature);
+
+			return file;
+		}
+
+		public string InsertDatesRange(string xml, DateTime start, DateTime end)
+		{
+			XElement file = XElement.Parse(xml);
+
+			var properties = file.Descendants("prop").Skip(2).ToList();
+			properties.First().SetAttributeValue("value", start.ToString("dd.MM.yyyy"));
+			properties.Last().SetAttributeValue("value", end.ToString("dd.MM.yyyy"));
+
+			return file.ToString(SaveOptions.DisableFormatting);
+		}
+	}
+}
---
+++ Tests/Funds.Tests/Bank/RequestBuilderTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace Funds.Tests.Bank
+{
+	public class RequestBuilderTests
+	{
+		private const string Request =
+			@"<?xml version=""1.0"" encoding=""UTF-8""?>
+			<request version=""1.0"">
+				<merchant>
+					<id>NULL</id>
+					<signature>NULL</signature>
+				</merchant>
+				<data>
+					<oper>cmt</oper>
+					<wait>0</wait>
+					<test>0</test>
+					<payment id="""">
+						<prop name=""card"" value=""NULL"" />
+						<prop name=""country"" value=""UA"" />
+						<prop name=""sd"" value=""NULL"" />
+						<prop name=""ed"" value=""NULL"" />
+					</payment>
+				</data>
+			</request>";
+
+		[Test]
+		public void InsertPersonalData_Always_()
+		{
+			
+		}
+	}
+}
