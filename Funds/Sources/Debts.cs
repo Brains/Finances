de0fa108a554df8384ccbc1e85487547d0ad8fbc
CompleteBrains <CompleteBrains@gmail.com>
2015-12-10 19:14:19 +0200

    Add Debts

---
+++ Funds/Sources/Debts.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Common;
+using Common.Storages;
+using static Common.Record;
+
+namespace Funds.Sources
+{
+	public class Debts : Base
+	{
+		private readonly IExpenses expenses;
+
+		public Debts(IExpenses expenses)
+		{
+			this.expenses = expenses;
+		}
+
+		public override void PullValue()
+		{
+			Value = 200;
+
+			var res = CalculateAmountsPerDude()
+        }
+
+		private Dictionary<Categories, Dictionary<string, decimal>> CalculateAmountsPerDude(IEnumerable<Record> records)
+		{
+			return (from record in records.Where(record => record.Type == Types.Debt)
+					group record by record.Category // Each Dude is a separate Category
+					into dude
+					select new
+					{
+						Name = dude.Key,
+						Total = (from record in dude
+								 group record by record.Description // Descriptions are either "In" or "Out" mean Direction of debt
+								 into grouped
+								 select new
+								 {
+									 Direction = grouped.Key,
+									 Total = grouped.Sum(record => record.Amount)
+								 })
+							.ToDictionary(kind => kind.Direction, kind => kind.Total)
+					}).ToDictionary(dude => dude.Name, dude => dude.Total);
+		}
+
+
+	}
+}
---
+++ Tests/Funds.Tests/Sources/DebtsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Common;
+using Funds.Sources;
+using NUnit.Framework;
+using static Common.Record.Types;
+using static Common.Record.Categories;
+
+namespace Funds.Tests.Sources
+{
+	public class DebtsTests
+	{
+		public static readonly Record[] Data =
+		{
+			new Record(100, Debt, Maxim,  "Out", new DateTime(1, 1, 1)),
+			new Record(100, Debt, Maxim,  "Out", new DateTime(1, 1, 1)),
+			new Record(100, Debt, Andrey, "Out", new DateTime(1, 1, 1)),
+			new Record(100, Debt, Andrey, "Out", new DateTime(1, 1, 1)),
+			new Record(100, Debt, Maxim,  "In",	 new DateTime(1, 1, 1)),
+			new Record(100, Debt, Andrey, "In",  new DateTime(1, 1, 1)),
+		};
+
+		[Test]
+		public void PullValue_Always_SetsValueToTotalDebt()
+		{
+			var debts = new Debts();
+
+			debts.PullValue();
+
+			Assert.That(debts.Value, Is.EqualTo(200));
+		}
+	}
+}
