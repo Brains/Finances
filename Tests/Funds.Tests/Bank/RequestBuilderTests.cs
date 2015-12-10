77f572d53fe5a3079110f885c82dbca252785d8c
CompleteBrains <CompleteBrains@gmail.com>
2015-12-10 06:28:32 +0200

    Add some Tests

--- Tests/Funds.Tests/Bank/RequestBuilderTests.cs
+++ Tests/Funds.Tests/Bank/RequestBuilderTests.cs
@@ -1,9 +1,16 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using System.Xml.Linq;
+using Funds.Bank;
+using NSubstitute;
+using NUnit.Framework;
+using static NSubstitute.Substitute;
 
 namespace Funds.Tests.Bank
 {
 	public class RequestBuilderTests
 	{
+		private IEncryption encryption;
+
 		private const string Request =
 			@"<?xml version=""1.0"" encoding=""UTF-8""?>
 			<request version=""1.0"">
@@ -24,10 +31,36 @@ namespace Funds.Tests.Bank
 				</data>
 			</request>";
 
-		[Test]
-		public void InsertPersonalData_Always_()
+		private RequestBuilder Create()
 		{
-			
+			encryption = For<IEncryption>();
+
+			return new RequestBuilder(encryption);
+		}
+
+		[Test]
+		public void InsertSecuredData_Always_InsertsSecuredDataIntoCorrespondingElements()
+		{
+			var builder = Create();
+			var data = new Data("1111", "", "9999");
+
+			var actual = builder.InsertSecuredData(Request, data);
+
+			var file = XElement.Parse(actual);
+			Assert.That(file.Descendants("id").Single().Value, Is.EqualTo("1111"));
+			Assert.That(file.Descendants("prop").First().LastAttribute.Value, Is.EqualTo("9999"));
+		}
+
+		[Test]
+		public void InsertSignature_Always_InsertsSignatureIntoCorrespondingElement()
+		{
+			var builder = Create();
+			var data = new Data("", "qwerty", "");
+			encryption.CalculateSignature(Arg.Any<string>()).Returns("MD5HASH");
+
+			var actual = builder.InsertSignature(Request, data);
+
+			Assert.That(actual.Descendants("signature").Single().Value, Is.EqualTo("MD5HASH"));
 		}
 	}
 }
