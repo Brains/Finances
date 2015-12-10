4ffe833f44098f725f0b10c4966d2d4b968b4343
CompleteBrains <CompleteBrains@gmail.com>
2015-12-10 15:29:23 +0200

    Add ResponceParser

---
+++ Tests/Funds.Tests/Bank/ResponceParserTests.cs
@@ -0,0 +1,18 @@
+﻿using Funds.Bank;
+using NUnit.Framework;
+
+namespace Funds.Tests.Bank
+{
+	public class ResponceParserTests
+	{
+		[Test]
+		public void ParseBalance_InvalidInput_ReturnsZero()
+		{
+			var parser = new ResponceParser();
+
+			var actual = parser.ParseBalance("Invalid");
+
+			Assert.That(actual, Is.EqualTo(0));
+		}
+	}
+}
