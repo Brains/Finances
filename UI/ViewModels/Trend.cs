93a0a53e6a35b19e9ddf410a68b6fa95ea48cbd9
CompleteBrains <CompleteBrains@gmail.com>
2015-12-12 18:04:17 +0200

    Transaction: nested class

--- UI/ViewModels/Trend.cs
+++ UI/ViewModels/Trend.cs
@@ -12,6 +12,13 @@ namespace UI.ViewModels
 {
 	public class Trend : Screen, IViewModel
 	{
+		public class Transaction
+		{
+			public decimal Amount { get; set; }
+			public DateTime Date { get; set; }
+			public string Description { get; set; }
+		}
+
 		public Trend(ISettings settings)
 		{
 			Operations = settings.PermanentOperations;
@@ -19,14 +26,14 @@ namespace UI.ViewModels
 
 		public PermanentOperation[] Operations { get; set; }
 
-		public IEnumerable<object> Transactions { get; set; }
+		public IEnumerable<Transaction> Transactions { get; set; }
 
 		protected override void OnInitialize()
 		{
 			base.OnInitialize();
 
 			ShiftTime(Operations);
-			Transactions = Calculate(0, Now, Now.AddMonths(3));
+			Transactions = Calculate(10000, Now, Now.AddMonths(3));
 		}
 
 		public void ShiftTime(PermanentOperation[] operations)
@@ -34,9 +41,9 @@ namespace UI.ViewModels
 			MoreLinq.MoreEnumerable.ForEach(operations, (operation, index) => operation.Start += FromHours(index + 1));
 		}
 
-		public IEnumerable<dynamic> Calculate(decimal startFunds, DateTime start, DateTime end)
+		public IEnumerable<Transaction> Calculate(decimal startFunds, DateTime start, DateTime end)
 		{
-			decimal accumulator = 1000;
+			decimal accumulator = startFunds;
 			var seed = new[] {accumulator};
 
 			var calendars = Operations.Select(operation => CalculateCalendar(start, end - start, operation));
@@ -54,14 +61,12 @@ namespace UI.ViewModels
 			var amounts = transactions.Select(transaction => accumulator += transaction.Amount);
 			amounts = seed.Concat(amounts);
 
-			var data = transactions.Zip(amounts, (transaction, amount) => new
+			return transactions.Zip(amounts, (transaction, amount) => new Transaction
 			{
 				Amount = amount,
-				transaction.Date,
-				transaction.Description
+				Date = transaction.Date,
+				Description = transaction.Description
 			});
-
-			return data;
 		}
 
 		private static IEnumerable<DateTime> CalculateCalendar(DateTime start, TimeSpan interval, PermanentOperation operation)
