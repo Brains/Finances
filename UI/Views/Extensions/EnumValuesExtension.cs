c48af6e1e053af73e93792df5977ca933c953198
CompleteBrains <CompleteBrains@gmail.com>
2015-12-14 12:44:14 +0200

    Add EnumValuesExtension for populate purely in XAML the ItemsSource of Controls for Enums

---
+++ UI/Views/Extensions/EnumValuesExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace UI.Views.Extensions
+{
+	public class EnumValuesExtension : MarkupExtension
+	{
+		private readonly Type enumType;
+
+		public EnumValuesExtension(Type enumType)
+		{
+			if (enumType == null)
+				throw new ArgumentNullException("enumType");
+			if (!enumType.IsEnum)
+				throw new ArgumentException("Argument enumType must derive from type Enum.");
+
+			this.enumType = enumType;
+		}
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return Enum.GetValues(enumType);
+		}
+	}
+}
