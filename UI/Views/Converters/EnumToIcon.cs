b6070d85c9e100348a4f9e74e118ecb27b70d032
CompleteBrains <CompleteBrains@gmail.com>
2015-12-14 21:36:19 +0200

    EnumTemplate: add icon

--- UI/Views/Converters/EnumToIcon.cs
+++ UI/Views/Converters/EnumToIcon.cs
@@ -1,25 +1,42 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
+using Common;
+using Mapping = System.Collections.Generic.Dictionary<string, string>;
 
 namespace UI.Views.Converters
 {
-	public class EnumToIcon : IValueConverter
+	public class EnumToIcon : Base
 	{
-		public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
+		readonly Mapping mapping = new Mapping
 		{
-			string name = value?.ToString();
+			["Expense"] = "appbar_arrow_down",
+			["Income"] = "appbar_arrow_up",
+			["Shared"] = "appbar_share",
+			["Debt"] = "appbar_arrow_up_down",
+			["Food"] = "appbar_food_silverware",
+			["House"] = "appbar_home",
+			["Health"] = "appbar_medical_pill",
+			["Other"] = "appbar_creditcard",
+			["General"] = "appbar_cart",
+			["Women"] = "appbar_gender_female",
+			["ODesk"] = "appbar_control_guide",
+			["Deposit"] = "appbar_money",
+			["Maxim"] = "appbar_camera",
+			["Andrey"] = "appbar_bike",
+		};
 
-			if (string.IsNullOrEmpty(name))
-				return null;
+		public override object Convert (object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var key = mapping[value.ToString()];
 
-			var visual = Application.Current.FindResource(name);
-			return visual as Visual;
+			return Application.Current.FindResource(key) as Visual;
 		}
 
-		public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
+		public override object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
 		{
 			return DependencyProperty.UnsetValue;
 		}
