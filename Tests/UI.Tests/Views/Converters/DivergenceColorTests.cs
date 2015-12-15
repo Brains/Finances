f5b94f574be5c3f40abd8bac02f56e302e5650ef
CompleteBrains <CompleteBrains@gmail.com>
2015-12-15 15:08:13 +0200

    Converters.DivergenceColor
    To visually highlight huge Divergence

---
+++ Tests/UI.Tests/Views/Converters/DivergenceColorTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Media;
+using NUnit.Framework;
+using UI.Views.Converters;
+
+namespace UI.Tests.Views.Converters
+{
+	public class DivergenceColorTests
+	{
+		[TestCase(0)]
+		[TestCase(1)]
+		[TestCase(1.4774)]
+		[TestCase(5)]
+		[TestCase(8.23)]
+		[TestCase(-8.23)]
+		[TestCase(-5)]
+		[TestCase(-1.4774)]
+		[TestCase(-0)]
+		public void Convert_AbsoluteValueBelowThreshold_ReturnsGrayBrush(decimal value)
+		{
+			DivergenceColor divergence = new DivergenceColor();
+
+			var actual = (SolidColorBrush) divergence.Convert(value, null, null, null);
+
+			Assert.That(actual.Color, Is.EqualTo(Colors.Gray));
+        } 
+
+		[TestCase(10)]
+		[TestCase(11)]
+		[TestCase(21.4774)]
+		[TestCase(995)]
+		[TestCase(78.23)]
+		[TestCase(-68.23)]
+		[TestCase(-15)]
+		[TestCase(-10.4774)]
+		[TestCase(-10)]
+		public void Convert_AbsoluteValueAboveThreshold_ReturnsOrangeRedBrush(decimal value)
+		{
+			DivergenceColor divergence = new DivergenceColor();
+
+			var actual = (SolidColorBrush) divergence.Convert(value, null, null, null);
+
+			Assert.That(actual.Color, Is.EqualTo(Colors.OrangeRed));
+        } 
+	}
+}
---
+++ UI/Views/Converters/DivergenceColor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace UI.Views.Converters
+{
+	public class DivergenceColor : Base
+	{
+		public int Threshold { get; } = 10;
+
+		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return  Math.Abs((decimal) value) < Threshold ? Brushes.Gray : Brushes.OrangeRed;
+
+		}
+	}
+}
