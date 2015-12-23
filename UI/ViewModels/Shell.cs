8d84c45fc8960c8bbbecfa8c3c594500897cdf86
CompleteBrains <CompleteBrains@gmail.com>
2015-12-23 15:51:49 +0200

    Reverts all previous commits in the branch
    This reverts commit 08d8634d129cabfb0c57624fe0c709980a8c35f2. (+4 squashed commit)
    
    Squashed commit:
    
    [2d290b9] Revert "Move DataGrid into Shell"
    
    This reverts commit a9a1e8509607b0b92c7f81bf599a7a9d5063458a.
    
    [9b25cba] Revert "Remove VirtualizationMode and ScrollUnit"
    
    This reverts commit 19fd39bd69d9b320cc4e66305d0fcd3940d34862.
    
    [e50bf58] Revert "Remove MahApps"
    
    This reverts commit bc963aa095f149cce7fbf71adea26e609f9b6170.
    
    [af3b0b4] Revert "Return Metro but replace DataGrid style"
    
    This reverts commit 1717479796ca77dec5fbc6018728cb922ec3253f. (+2 squashed commit)
    
    Squashed commit:
    
    [4689415] Revert "Remove MahApps"
    
    This reverts commit bc963aa095f149cce7fbf71adea26e609f9b6170.
    
    [0a94ed9] Revert "Return Metro but replace DataGrid style"
    
    This reverts commit 1717479796ca77dec5fbc6018728cb922ec3253f.

--- UI/ViewModels/Shell.cs
+++ UI/ViewModels/Shell.cs
@@ -1,38 +1,20 @@
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Windows.Data;
 using Caliburn.Micro;
-using Common;
-using Common.Storages;
-using UI.Views.Converters;
 
 namespace UI.ViewModels
 {
 	public interface IShell {}
 
-	public class Shell : PropertyChangedBase, IShell
+	public class Shell : Conductor<IScreen>.Collection.OneActive, IShell
 	{
 		public IScreen SelectedItem { get; set; }
-		public Shell(IExpenses expenses)
+		public Shell(IScreen[] screens)
 		{
-			List = expenses.Records;
+			DisplayName = "Finances";
 
-			ConfigureView();
+			Items.AddRange(screens);
 
-		}
-		public IEnumerable<Record> List { get; }
-		private void ConfigureView()
-		{
-			var view = CollectionViewSource.GetDefaultView(List);
-
-			view.SortDescriptions.Clear();
-			view.SortDescriptions.Add(new SortDescription("Date", ListSortDirection.Descending));
-
-			view.GroupDescriptions.Clear();
-			view.GroupDescriptions.Add(new PropertyGroupDescription("Date", new GroupingMonth()));
-			view.GroupDescriptions.Add(new PropertyGroupDescription("Date", new GroupingDate()));
-		}
-
+			ActivateItem(Items[0]);
+        }
 	}
 }
