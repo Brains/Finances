d5f313861134d0fbee38da534010a42415e2bf2d
CompleteBrains <CompleteBrains@gmail.com>
2015-12-26 10:38:31 +0200

    Add ToolTip for Debts only (Hack with Dynamic)

---
+++ UI/Views/Converters/FundsToolTip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace UI.Views.Converters
+{
+	public class FundsToolTip : Base
+	{
+		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			dynamic source = value;
+
+			if (source.Name == "Debts")
+				return parameter;
+
+			return DependencyProperty.UnsetValue;
+		}
+	}
+}
