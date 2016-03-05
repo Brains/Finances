efbb6408c19256c11b3d382c7731c33e67627644
CompleteBrains <CompleteBrains@gmail.com>
2016-03-05 09:57:43 +0200

    Add .cs

---
+++ UI/Services/Amount.cs
@@ -0,0 +1,42 @@
+﻿using Common;
+using UI.Views.Converters;
+
+namespace UI.Services
+{
+	public interface IAmountFactory
+	{
+		IAmount Create(Record.Types type);
+	}
+
+	public interface IAmount
+	{
+		decimal Value { get; set; }
+		decimal Total { get; }
+	}
+
+	public class Amount : IAmount
+	{
+		public virtual decimal Value { get; set; }
+
+		public virtual decimal Total => Value;
+	}
+
+	public class SharedAmount : Amount
+	{
+		private readonly int customers;
+		private decimal value;
+
+		public SharedAmount(ISettings settings)
+		{
+			customers = settings.Customers;
+		}
+
+		public override decimal Value
+		{
+			get { return value; }
+			set { this.value = value / customers; }
+		}
+
+		public override decimal Total => Value * customers;
+	}
+}
