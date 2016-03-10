5d59bb75526cfa8d4ede3579eddf54f9e9b1a7d7
CompleteBrains <CompleteBrains@gmail.com>
2016-03-10 21:53:31 +0200

    ChangedPrimaryFormAmount_Never_CallsSubtract

--- Tests/UI.Tests/Services/SubtractorTests.cs
+++ Tests/UI.Tests/Services/SubtractorTests.cs
@@ -1,7 +1,49 @@
-﻿namespace UI.Tests.Services
+﻿using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using UI.Interfaces;
+using UI.Services;
+
+namespace UI.Tests.Services
 {
 	public class SubtractorTests
 	{
-		 
+		[Test]
+		public void ChangedPrimaryFormAmount_WithSingleForm_DoesNotCallSubtract()
+		{
+			Subtractor subtractor = new Subtractor();
+			IForm primary = Substitute.For<IForm>();
+			subtractor.Add(primary);
+
+			primary.Amount = "5";
+
+			primary.DidNotReceive().Subtract(Arg.Any<IEnumerable<IForm>>());
+		} 
+		[Test]
+		public void ChangedPrimaryFormAmount_WithTwoForms_DoesNotCallSubtract()
+		{
+			Subtractor subtractor = new Subtractor();
+			IForm primary = Substitute.For<IForm>();
+			subtractor.Add(primary);
+			subtractor.Add(Substitute.For<IForm>());
+
+			primary.Amount = "5";
+
+			primary.DidNotReceive().Subtract(Arg.Any<IEnumerable<IForm>>());
+		} 
+
+		[Test]
+		public void ChangedPrimaryFormAmount_Never_CallsSubtract()
+		{
+			Subtractor subtractor = new Subtractor();
+			IForm first = Substitute.For<IForm>();
+			IForm second = Substitute.For<IForm>();
+			subtractor.Add(first);
+			subtractor.Add(second);
+
+			first.Amount = "5";
+
+			first.DidNotReceive().Subtract(Arg.Any<IEnumerable<IForm>>());
+		} 
 	}
 }
--- UI/Services/Subtractor.cs
+++ UI/Services/Subtractor.cs
@@ -1,7 +1,20 @@
-﻿namespace UI.Services
+﻿using System.Collections.Generic;
+using UI.Interfaces;
+
+namespace UI.Services
 {
 	public class Subtractor
 	{
-		 
+		private readonly IList<IForm> forms;
+
+		public Subtractor()
+		{
+			forms = new List<IForm>();
+		}
+
+		public void Add(IForm form)
+		{
+			forms.Add(form);
+		}
 	}
 }
