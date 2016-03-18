8acb212ad8c4e80e0e616aff744a51606c0dd8d3
Solar Brains <solarbrains@live.com>
2016-03-18 04:24:56 +0200

    Add ViewModels.Fund

---
+++ UI/ViewModels/Fund.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using Caliburn.Micro;
+using Common;
+using UI.Services;
+
+namespace UI.ViewModels
+{
+    public class Fund : PropertyChangedBase, IFund
+    {
+        private readonly IFundsSource source;
+
+        public Fund(IFundsSource source)
+        {
+            this.source = source;
+            this.source.Update += value => Value = value;
+        }
+
+        public string Name { get; set; }
+        public bool IsEditable { get; set; }
+
+        [Notify]
+        public decimal Value { get; set; }
+
+        public void PullValue() => source.PullValue();
+    }
+
+    public interface IFund : INotifyPropertyChanged
+    {
+        decimal Value { get; set; }
+		void PullValue();
+    }
+}
