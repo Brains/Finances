01e8ac277decdd3cf5f4b5fe9c52e1e719509972
Solar Brains <solarbrains@live.com>
2016-03-31 08:27:33 +0300

    Remove old implementation

--- Tests/UI.Tests/ViewModels/TrendTests.cs
+++ Tests/UI.Tests/ViewModels/TrendTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Common;
+using Common.Storages;
 using MoreLinq;
 using NUnit.Framework;
 using UI.ViewModels;
@@ -14,80 +15,7 @@ namespace UI.Tests.ViewModels
 		{
 			var settings = For<ISettings>();
 
-			return new Trend(settings);
-		}
-
-		[Test]
-		public void Calculate_Always_CalculatesFundsAfterEachOccurrenceOfOperation()
-		{
-			var trend = Create();
-			trend.Operations = new[]
-			{
-				new PermanentOperation(-100, new DateTime(1, 1, 1), TimeSpan.FromDays(3), "Food")
-			};
-			var initial = 1000;
-			var start = new DateTime(2, 1, 1);
-
-			var actual = trend.Calculate(initial, start, start.AddMonths(1)).ToList();
-
-			var expectedAmounts = new[] {1000, 900, 800, 700, 600, 500, 400, 300, 200, 100};
-			var expectedDays = new[] {1, 4, 7, 10, 13, 16, 19, 22, 25, 28};
-			Assert.That(actual.Select(item => item.Amount), Is.EquivalentTo(expectedAmounts));
-			Assert.That(actual.Select(item => item.Date.Day), Is.EquivalentTo(expectedDays));
-			Assert.That(actual.Select(item => item.Description), Has.All.EqualTo("Food"));
-			Assert.That(actual.First().Amount, Is.EqualTo(initial));
-		}
-
-		[Test]
-		public void CalculateCalendar_Always_ReturnsCalendarWithAllTransactionsUntilEndDate()
-		{
-			var trend = Create();
-			var start = new DateTime(2, 1, 1);
-			var end = start.AddMonths(1);
-			var operation = new PermanentOperation(-100, new DateTime(1, 1, 1), TimeSpan.FromDays(3), "Test");
-
-			var actual = trend.CalculateCalendar(start, end - start, operation).ToList();
-
-			var intervals = Enumerable.Zip(actual.Skip(1), actual, (a, b) => a - b);
-			Assert.That(actual.Count, Is.EqualTo(10));
-			Assert.That(intervals, Has.All.EqualTo(TimeSpan.FromDays(3)));
-			Assert.That(actual.Select(item => item.Date), Has.All.GreaterThanOrEqualTo(start).And.LessThan(end));
-		}
-
-		[Test]
-		public void ShiftTime_Always_AddsTimeShiftForOperations()
-		{
-			var trend = Create();
-			trend.Operations = new[]
-			{
-				new PermanentOperation(-100, new DateTime(1, 1, 1), TimeSpan.FromDays(3), "Test"),
-				new PermanentOperation(-100, new DateTime(1, 1, 1), TimeSpan.FromDays(3), "Test"),
-				new PermanentOperation(-100, new DateTime(1, 1, 1), TimeSpan.FromDays(3), "Test")
-			};
-
-			trend.ShiftTime(trend.Operations);
-
-			var hours = trend.Operations.Select(o => o.Start.Hour);
-			Assert.That(hours, Is.EquivalentTo(new[] {1, 2, 3}));
-		}
-
-		[Test]
-		public void TransactionsGetter_Always_ReturnsSameAmountsList()
-		{
-			var trend = Create();
-			trend.Operations = new[]
-			{
-				new PermanentOperation(-100, new DateTime(1, 1, 1), TimeSpan.FromDays(3), "Food")
-			};
-			var initial = 1000;
-			var start = new DateTime(2, 1, 1);
-
-			trend.Transactions = trend.Calculate(initial, start, start.AddMonths(1));
-
-			var expected = new[] { 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100 };
-			Assert.That(trend.Transactions.Select(item => item.Amount), Is.EquivalentTo(expected));
-			Assert.That(trend.Transactions.Select(item => item.Amount), Is.EquivalentTo(expected));
-			Assert.That(trend.Transactions.Select(item => item.Amount), Is.EquivalentTo(expected));
+			return new Trend(For<IExpenses>(), settings);
 		}
 	}
 }
--- UI/ViewModels/Trend.cs
+++ UI/ViewModels/Trend.cs
@@ -35,53 +35,8 @@ namespace UI.ViewModels
 		protected override void OnInitialize()
 		{
 			base.OnInitialize();
-
-			ShiftTime(Operations);
-			Transactions = Calculate(5000, Today, Today.AddMonths(2));
 		}
 
-		public IList<Transaction> Calculate(decimal startFunds, DateTime start, DateTime end)
-		{
-			decimal accumulator = startFunds;
-			var seed = new[] {accumulator};
-
-			var calendars = Operations.Select(operation => CalculateCalendar(start, end - start, operation));
-
-			var transactions = Operations.Zip(calendars,
-			                                  (operation, dates) => dates.Select(date => new
-			                                  {
-				                                  operation.Amount,
-				                                  Date = date,
-				                                  operation.Description
-			                                  }))
-			                             .SelectMany(operation => operation.ToArray())
-			                             .OrderBy(transaction => transaction.Date);
-
-			var amounts = transactions.Select(transaction => accumulator += transaction.Amount);
-			amounts = seed.Concat(amounts);
-
-			return transactions.Zip(amounts, (transaction, amount) => new Transaction
-			{
-				Amount = amount,
-				Date = transaction.Date,
-				Description = transaction.Description
-			}).ToList();
-		}
-
-		public IEnumerable<DateTime> CalculateCalendar(DateTime start, TimeSpan interval, PermanentOperation operation)
-		{
-			var period = operation.Period.Ticks;
-			var quantity = (int) (interval.Ticks / period);
-
-			var calendar = Range(0, quantity).Select(index => operation.Start + FromTicks(index * period));
-			
-			return calendar.Where(date => date > start);
-		}
-
-		public void ShiftTime(PermanentOperation[] operations)
-		{
-			MoreLinq.MoreEnumerable.ForEach(operations, (operation, index) => operation.Start += FromHours(index + 1));
-		}
 
 		public class Transaction
 		{
